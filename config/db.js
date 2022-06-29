7ffb8adefe620375eb9be08c990570b498bde7d1
Durgesh Agnihotri <agnihotridurgesh123@gmail.com>
2022-06-29 13:17:26 +0530

    initial

---
+++ config/db.js
@@ -0,0 +1,68 @@
+require('dotenv').config();// required to use variable of .env file
+
+const mongoose = require('mongoose');
+const URI =process.env.MONGO_CONNECTION_URL;
+
+function connectDB() {
+    // Database connection 🥳
+    mongoose.connect(URI, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+        }, err => {
+        if(err) throw err;
+        console.log('Database Connected !')
+        });
+}
+// TsDzr9erVoni1m7C
+
+module.exports = connectDB;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// require('dotenv').config();
+// const mongoose=require('mongoose');
+// function connectDB(){
+//     // Dtatbase Connection
+//     mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+//     const connection = mongoose.connection;
+
+//     connection.once('open',()=>{
+//         console.log('Database connected .');
+//     }).catch(err=>{
+//         console.log('Connection Failed');
+//     })
+// }
+
+
+
+
+// module.export=connectDB;
---
+++ server.js
@@ -0,0 +1,31 @@
+// express server
+const express=require('express');
+const app =express();
+const path=require('path');
+
+
+const PORT=process.env.PORT || 3000;
+app.use(express.static('public'))
+app.use(express.json());
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`);
+})
+// main ke nneche
+// "scripts": {
+  //   "test": "echo \"Error: no test specified\" && exit 1"
+  // },
+
+
+  // database ko server se connect krne ke liye
+  const connectDB=require('./config/db');
+  connectDB();
+
+
+  // Routes
+  app.use('/api/files',require('./routes/files'));
+  app.use('/files',require('./routes/show'));
+  app.use('/files/download',require('./routes/download'));
+
+  // Template engine
+  app.set('views',path.join(__dirname,'/views'));
+  app.set('view engine','ejs');
