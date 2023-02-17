72fcba311d715d41d3144f2202139211b3177d99
Durgesh Agnihotri <agnihotridurgesh123@gmail.com>
2023-02-17 19:47:29 +0530

    initial

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express=require('express');
 const app =express();
 const path=require('path');
 const cors=require('cors');
+const auth=require('./middleware/auth');
+const bcrypt=require('bcrypt');
 
 // Cors
 
@@ -27,9 +29,25 @@ app.listen(PORT,()=>{
   const connectDB=require('./config/db');
   connectDB();
   // Routes
+  app.use('/register',require('./routes/register'))// to register user
+  app.use('/login',require('./routes/login'))// to login user
   app.use('/api/files',require('./routes/files'));
   app.use('/files',require('./routes/show'));
   app.use('/files/download',require('./routes/download'));
+
+  app.get('/home',auth,(req,res)=>{
+    res.render('home');
+});
+
+app.get('/logout',auth,async (req,res)=>{
+    try{
+        res.clearCookie('jwt');
+        res.redirect('/');
+    }catch(error){
+        res.status(500).send(error);
+    }
+
+})
 
   // Template engine
   app.set('views',path.join(__dirname,'/views'));
